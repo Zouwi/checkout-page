d46cfce54beed0819433d0035fff6dacabcc8ee3
Zouwi <zoegabet89@gmail.com>
2025-03-05 16:51:43 +0100

    Build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/checkout-page',
-    assetPrefix: "/checkout-page/",
   };
   
   export default nextConfig;
